6f4056e2ceae98527d7683dc4a04b2a413138ec9
Majidu <onyangomajidu@gmail.com>
2026-07-27 17:27:40 +0300

    some changes

--- laravel/resources/views/topics.blade.php
+++ laravel/resources/views/topics.blade.php
@@ -1292,7 +1292,8 @@
                 .catch(() => {});
         }
 
-        setInterval(pollPosts, 3000);
+        pollPosts();
+        setInterval(pollPosts, 1500);
 
         // Typing indicator via Echo whisper (kept if Echo is available)
         if (typeof window.Echo !== 'undefined') {
